bdbf90eb34d77a05c9dda7dcd999359bb9d37af3
Sho8000 <yoshikichi8000@gmail.com>
2025-05-22 16:26:37 -0700

    updated the meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ export default function Home() {
   return (
     <>
       <SeoHead
-        title="SampleRestaurant | This is TEST"
-        description="You should Go There!!!"
+        title="SampleRestaurant | This is TEST sample restaurant page to check SEO"
+        description="Explore a sample restaurant website based in Vancouver, built for SEO testing and demo purposes."
         ogImage="https://postcardimage.s3.us-east-2.amazonaws.com/7b9c6fea-9a43-4bb2-b984-174b932294d3-Zustand_Logo_90_90.png"
         url="https://seo-test-delta-ruddy.vercel.app/"
         googleVerification="google1574aaf162ed4e0c.html"
       />
-      <h1>This is test page for SEO</h1>
+      <h1>Welcome to Sample Restaurant — Best Test Page for SEO</h1>
+      <p>This page is a great example for learning SEO, restaurant keywords, and how to optimize your site for search engines.</p>
     </>
   );
 }
