aee3f49673aa9d1f1c94e27f2d3d83de46453244
Sho8000 <yoshikichi8000@gmail.com>
2025-05-22 13:02:58 -0700

    added googleInfo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         title="SampleRestaurant | This is TEST"
         description="You should Go There!!!"
         ogImage="https://postcardimage.s3.us-east-2.amazonaws.com/7b9c6fea-9a43-4bb2-b984-174b932294d3-Zustand_Logo_90_90.png"
-        url="https://yourdomain.com/"
+        url="https://seo-test-delta-ruddy.vercel.app/"
       />
       <h1>This is test page for SEO</h1>
     </>
