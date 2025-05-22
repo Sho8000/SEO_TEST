a06d6b9ff31fc1dfea3d53b5a28b1017d88dda56
Sho8000 <yoshikichi8000@gmail.com>
2025-05-22 12:54:37 -0700

    first commit

---
+++ src/app/(components)/SeoHead.tsx
@@ -0,0 +1,27 @@
+// components/SeoHead.tsx
+import Head from 'next/head';
+
+interface SeoHeadProps {
+  title: string;
+  description: string;
+  ogImage?: string;
+  url?: string;
+}
+
+const SeoHead: React.FC<SeoHeadProps> = ({ title, description, ogImage, url }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {ogImage && <meta property="og:image" content={ogImage} />}
+      {url && <meta property="og:url" content={url} />}
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="robots" content="index, follow" />
+    </Head>
+  );
+};
+
+export default SeoHead;
