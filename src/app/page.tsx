a88c250ff69fef4f830a4df127eb500859732a98
Sho8000 <yoshikichi8000@gmail.com>
2025-05-23 14:15:36 -0700

    chenged the vercel url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         title="SampleRestaurant | This is TEST sample restaurant page to check SEO"
         description="Explore a sample restaurant website based in Vancouver, built for SEO testing and demo purposes."
         ogImage="https://postcardimage.s3.us-east-2.amazonaws.com/7b9c6fea-9a43-4bb2-b984-174b932294d3-Zustand_Logo_90_90.png"
-        url="https://seo-test-delta-ruddy.vercel.app/"
+        url="https://vancouver-sample-seo-test-restaurant.vercel.app/"
         googleVerification="google1574aaf162ed4e0c.html"
       />
       <h1>Welcome to Sample Restaurant — Best Test Page for SEO</h1>
